c024cd0c11cdc332c42b5ff27101ca7a4ac0f34e
Julien Cheype <cheype@gmail.com>
2017-04-07 17:28:17 +0200

    update angular version

--- src/lib/toolbar.module.ts
+++ src/lib/toolbar.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { DropdownModule } from 'ng2-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { DiscoveryModule } from 'vidal-ng2-discovery';
 import { SesameModule } from 'vidal-ng2-sesame';
@@ -10,7 +10,7 @@ import { SesameModule } from 'vidal-ng2-sesame';
 import { ToolbarComponent } from './toolbar.component';
 
 @NgModule({
-    imports: [FormsModule, CommonModule, SesameModule, DiscoveryModule, DropdownModule
+    imports: [FormsModule, CommonModule, SesameModule, DiscoveryModule, BsDropdownModule
 ],
     exports: [ToolbarComponent],
     declarations: [ToolbarComponent]
